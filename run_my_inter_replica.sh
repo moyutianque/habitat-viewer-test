8aeac5a3c0005cd40a20f728dc4cc0115a56683a
zeke96 <15305575+moyutianque@users.noreply.github.com>
2023-01-05 09:36:49 +0100

    Add files via upload

---
+++ run_my_inter_replica.sh
@@ -0,0 +1,6 @@
+scene_path=data/replica_v1/apartment_2/habitat/mesh_semantic.ply 
+# scene_path=~/large_data/3D/habitat/scene_datasets/17DRP5sb8fy/17DRP5sb8fy.glb
+python viewer.py --scene $scene_path --dataset ./ui_data/mp3d.scene_dataset_config.json
+
+# echo "START transforming to nerf used data format"
+# python ../convert_poses.py
